b73925b054cb6aa487bddb9645640e1552b44ea7
unknown <zz240614@gmail.com>
2025-02-10 02:50:53 +0100

    Starting my Project FoodExpsse With Architucture Mvc structure

---
+++ App/Routes/Route.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\Routes;
+
+
+class Route {
+    public $path;
+    public $callable;
+
+    public function __construct($path, $callable)
+    {
+        $this->path = $path;
+        $this->callable = $callable;
+    }
+}
+
---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Controller\IndexController;
+use App\Routes\Router;
+
+require './vendor/autoload.php';
+session_start();
+
+$router = new Router($_SERVER['REQUEST_URI']);
+
+
+
+// routes 
+$router->get('/', [IndexController::class, 'index']);
+
+$router->run();
